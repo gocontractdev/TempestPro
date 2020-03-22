3616d75fb1ee584cf4aaef21cfe343d4b36f7750
amir <javaher21@gmail.com>
2020-03-22 11:47:59 +0100

    under construction

--- database/migrations/2020_03_22_021833_create_permissions_table.php
+++ database/migrations/2020_03_22_021833_create_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInteractionsTable extends Migration
+class CreatePermissionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateInteractionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('interactions', function (Blueprint $table) {
+        Schema::create('permissions', function (Blueprint $table) {
             $table->id();
             $table->string('key')->unique();
-            //$table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ class CreateInteractionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('interactions');
+        Schema::dropIfExists('permissions');
     }
 }
